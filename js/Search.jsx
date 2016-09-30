6c790817c56dd3a247dbf9919b83ff9b2bc2b1a6
Nicholas VanVoorthuysen <nicholas.vanvoorthuysen@gmail.com>
2016-09-30 14:50:43 -0400

    starting from react intro course project at end of state lesson section

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,14 +2,33 @@ const React = require('react')
 const data = require('../public/data')
 const ShowCard = require('./ShowCard')
 
-const Search = () => (
-  <div className='container'>
-    <div className='shows'>
-      {data.shows.map((s) => (
-        <ShowCard show={s} key={s.imdbID} />
-      ))}
-    </div>
-  </div>
-)
+const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'search'
+    }
+  },
+  handleSearchTermEvent (event) {
+    this.setState({ searchTerm: event.target.value })
+  },
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>nvanvideo</h1>
+          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
+        </header>
+        <div className='shows'>
+          {data.shows
+            .filter((show) => `${show.title} ${show.description}`.toUpperCase()
+            .indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map((s) => (
+              <ShowCard show={s} key={s.imdbID} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Search
